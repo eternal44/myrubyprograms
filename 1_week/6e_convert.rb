9b753cebb4ac33a44e3dfb002282706f49891eb3
James Youn <jamesyoun710@gmail.com>
2015-08-09 15:57:16 -0700

    Redesign code to fit exercise & refactor i/o.
    Redesign old method to fit exercise paramaters (f to c).
    Refactor method so it only returns value.
    Our tests only ensure the conversion is correct to 4 decimal places
    Include running code in the file but guarded with a library guard.
    Add string formatting.

--- 1_week/6e_convert.rb
+++ 1_week/6e_convert.rb
@@ -1,8 +1,29 @@
-def celcius_to_fahrenheit(c)
-  f = c * 1.8 + 32
-  puts format("%.2f", f)
+# The assignment is to create a method named convert that converts
+# a value given as F to Celcius
+# doctest: convert method takes a number and converts it to Celcius
+# >> convert(-40)
+# => -40
+# doctest: convert boiling point
+# >> convert(212)
+# => 100
+# doctest: convert freezing point
+# >> convert(32)
+# => 0
+# doctest: convert body temperature
+# >> convert(98.6)
+# => 37
+# doctest: convert 98
+# >> to_celcius(98).round(4)
+# => 36.6667
+def convert(_F_temp)
+  _Celcius = (_F_temp - 32.0) * 5 / 9
 end
+alias to_celcius convert
 
-c = 32
-celcius_to_fahrenheit(c)
+# This is called the Library Guard
+if __FILE__  == $PROGRAM_NAME
+  [-40, 0, 32, 98, 98.6, 100, 212].each do |_F|
+    puts("%.2f in F degrees in celcius is %.2f." % [to_celcius(_F), _F])
+  end
+end
 
