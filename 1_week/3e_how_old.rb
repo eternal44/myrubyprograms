b99ea017c33b6bba73c2b15dbb6982ec61d9fcf2
James Youn <jamesyoun710@gmail.com>
2015-08-09 12:58:22 -0700

    How Old exercise years from seconds
    Using rubydoctest as our testing tool, we write a method to get the age
    in years from a value in seconds.

--- 1_week/3e_how_old.rb
+++ 1_week/3e_how_old.rb
@@ -1,18 +1,18 @@
-def age_calculator(x)
-
+# doctest:  age_calcuator returns decimal age in years
+# >> age_in_years(9.79e8).round(2)
+# => 31.04
+def age_in_years(age_in_seconds)
   seconds = 60.0
   minutes = 60.0
   hours = 24.0
   days = 365.0
 
-  age = x / seconds / minutes / hours / days
-
-  puts format("%.2f", age)
+  age_in_seconds / seconds / minutes / hours / days
 end
 
-seconds = 979_000_000
-age_calculator(seconds)
-
-
-
-
+if __FILE__ == $PROGRAM_NAME
+  seconds = 979_000_000
+  puts "If you are #{seconds} seconds old, then you are "
+  print format("%.2f", age_in_years(seconds))
+  puts " seconds old."
+end
