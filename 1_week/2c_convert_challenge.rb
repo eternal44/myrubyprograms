43a7e16c0aa81dddbd2768453c55a18c5844d24c
James Youn <jamesyoun710@gmail.com>
2015-08-10 20:48:05 -0700

    First commit for 1_week/2c_convert_challege.rb
    Stage test for Celcius to Fahrenheit conversion.

---
+++ 1_week/2c_convert_challenge.rb
@@ -0,0 +1,10 @@
+# doctest: convert should give correct results
+# >> c_to_f = "C to F"
+# >> value = 0
+# >> convert_temperature(c_to_f, value)
+# => "32 degrees F"
+
+def convert_temperature(conversion_to_make, value)
+  "32 degrees F"
+  
+end
