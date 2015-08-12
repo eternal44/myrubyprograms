910886d6cc063077d4d9d8c779f88bf2c20988dc
James Youn <jamesyoun710@gmail.com>
2015-08-12 01:28:07 -0700

    Clean up refactor
    Improve library guard to loop through an array of ages
    clear out comments

--- 1_week/1c_year_months.rb
+++ 1_week/1c_year_months.rb
@@ -13,38 +13,37 @@ def seconds_to_years_and_months(age_in_seconds)
   years = 365
   months = 12.0
 
-    age_in_months = (age_in_seconds / minutes / hours / days / years * months)
-    age_in_months = age_in_months.to_i
+  age_in_months = (age_in_seconds / minutes / hours / days / years * months)
+  age_in_months = age_in_months.to_i
 
-    if age_in_months % 12 != 0
-      input_months = (age_in_months % 12)
-      # input_months = " and #{input_months} months"
-    else
-      input_months = 0
-    end
+  if age_in_months % 12 != 0
+    input_months = (age_in_months % 12)
+  else
+    input_months = 0
+  end
 
-    input_years = age_in_months / 12
+  input_years = age_in_months / 12
 
-    return input_years, input_months
-    # "I'm #{input_years} years#{input_months} old."
+  return input_years, input_months
 end
 
-
+# Library guard
 if __FILE__ == $PROGRAM_NAME
-  # seconds = [979000000, 2158493738, 246144023, 1270166272, 1025600095]
+  seconds = [979000000, 2158493738, 246144023, 1270166272, 1025600095]
 
-  seconds = 979_000_000
-  formatting = '%<have>s %<seconds>i %<second_part>s'\
-               '%<converted_years>3d %<years>s %<months>i %<last_part>s'
-  string_parts = {
-    have: "If you have ",
-    seconds: seconds,
-    second_part: "seconds then you are",
-    converted_years: seconds_to_years_and_months(seconds)[0],
-    years: "years and",
-    months: seconds_to_years_and_months(seconds)[1],
-    last_part: "months old." 
+  seconds.each do |age|
+    formatting = '%<have>s %<seconds>i %<second_part>s'\
+      '%<converted_years>3d %<years>s %<months>i %<last_part>s'
+    string_parts = {
+      have: "If you have ",
+      seconds: age,
+      second_part: "seconds then you are",
+      converted_years: seconds_to_years_and_months(age)[0],
+      years: "years and",
+      months: seconds_to_years_and_months(age)[1],
+      last_part: "months old." 
 
-  }
-  puts formatting % string_parts
+    }
+    puts formatting % string_parts
+  end
 end
