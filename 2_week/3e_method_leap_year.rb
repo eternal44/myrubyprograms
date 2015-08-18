64a97fdb62e773e02898f954c0a8c476c1936f76
James Youn <jamesyoun710@gmail.com>
2015-08-17 18:46:40 -0700

    3e_method_leap_year.rb first draft
    Add leap year filter
    No minute calculations yet

--- 2_week/3e_method_leap_year.rb
+++ 2_week/3e_method_leap_year.rb
@@ -1,10 +1,26 @@
-# doctest: test test
-# >> check_leap_year(10)
-# => "hello"
+# doctest: correct century leap year
+# >> leap_year?(400)
+# => true
+# doctest: false century leap year
+# >> leap_year?(300)
+# => false
+# doctest: correct normal leap year
+# >> leap_year?(304)
+# => true
+# doctest: normal year tests false
+# >> leap_year?(1993)
+# => false
 
-def check_leap_year(year)
-  "hello"
+def leap_year?(year)
+  if year % 400 == 0
+    true
+    # yield calc minutes
+  elsif year.to_s[-2..-1] != "00" && year % 4 == 0
+    true
+    # yield calc minutes
+  else
+    false
+  end
 end
 
-check_leap_year(1) {puts 'wutsup'}
 
