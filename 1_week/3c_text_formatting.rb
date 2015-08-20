2b709ddd9ea589f7e487820b36a232b478859a3a
James Youn <jamesyoun710@gmail.com>
2015-08-19 22:01:05 -0700

    WIP: First refactoring & rebuild
    Better variable names
    Private method
    Acceptance tests
    Method signature

--- 1_week/3c_text_formatting.rb
+++ 1_week/3c_text_formatting.rb
@@ -1,8 +1,13 @@
 # This method generates a multiplication table.
+
 class Table
-  def generate_table(x, y='y', z='y')
+  def generate_table(
+    table_size,
+    title="Times table to #{table_size}",
+    decoration = '')
+    result = ""
     # set up parameters for table
-    arr2 = arr1 = (1..x).to_a 
+    arr2 = arr1 = (1..table_size).to_a
     col_size = (arr2[-1] * arr2[-1]).to_s.size
 
     table = ""
@@ -16,67 +21,90 @@ class Table
         product = y * z
         num_size = product.to_s.size
 
-        if num_size < col_size
-          spacing = " " * (col_size - num_size) 
-        else
-          spacing = ""
-        end
-        row << " " << spacing << (y*z).to_s
+        row << " " << spacing(num_size, col_size) << (y*z).to_s
       end
+
       table << row << "\n"
       length = row.size
-      border = " " + "=" * length
+      border = decoration.to_s * (length + 1) + "\n"
     end
 
     # table layout options
-    if y == "y" && length > 16 
-      header = " " * (length / 2 - 8) + " Times Table to #{x}"
-      puts header
+    if title && length > 16
+      header = " " * (length / 2 - 8) + title + "\n"
+      result << header
     elsif length < 16
-      header = " Times Table to #{x}"
-      puts header
+      header = title + "\n"
+      result << header
     else
-      header = ""
+      header = "\n"
     end
 
-    puts border if z == "y"
-    puts table
+    result << border unless decoration.empty?
+    result << table
+    result << border unless decoration.empty?
+    result
+  end
+
+  private
+  def spacing(num_size, col_size)
+        num_size < col_size ?
+           " " * (col_size - num_size) : ""
+
   end
 end
 
-#########
-# TEST  #
-#########
+if __FILE__ == $PROGRAM_NAME
+  #########
+  # TEST  #
+  #########
 
-#########
-# INPUT #
-#########
+  #########
+  # INPUT #
+  #########
 
-# number input
-puts "Enter a number you want to generate a times table for"
-num = gets.to_i
-puts "Invalid entry" unless num.is_a?(Integer) 
+  # number input
+  puts "Enter a number you want to generate a times table for"
+  num = gets.to_i
+  puts "Invalid entry" unless num.is_a?(Integer)
 
-# header input
-puts "Include Header? Enter 'y' or 'n'."
-header = gets.chomp
+  # header input
+  puts "Include Header? Enter 'y' or 'n'."
+  header = gets.chomp
 
-if header != "y" && header != "n"
-  puts "Invalid entry.  Use 'y' or 'n'." 
-  exit
+  if header != "y" && header != "n"
+    puts "Invalid entry.  Use 'y' or 'n'."
+    exit
+  end
+
+  # border input
+  puts "Include border?  Enter 'y' or 'n'."
+  border = gets.chomp
+
+  if border != "y" && border != "n"
+    puts "Invalid entry.  Use 'y' or 'n'."
+    exit
+  end
+
+  ##############
+  # RUN SCRIPT #
+  ##############
+
+  generate_table(num, header, border)
 end
-
-# border input
-puts "Include border?  Enter 'y' or 'n'."
-border = gets.chomp
-
-if border != "y" && border != "n"
-  puts "Invalid entry.  Use 'y' or 'n'." 
-  exit
-end
-
-##############
-# RUN SCRIPT #
-##############
-
-generate_table(num, header, border)
+## This is a pass / fail test.  To get get graded test just load it
+## in irb and run
+# doctest: Acceptance test & with customized decoration
+# >> Table.new().generate_table(3,"", "+")
+# >> standard = "\n+++++++\n 1 2 3\n 2 4 6\n 3 6 9\n+++++++\n"
+# >> Table.new().generate_table(3,"", "+") == standard
+# => true
+# doctest: Acceptance test with title and decoration
+# >> standard = "Times table to 3\n 1 2 3\n 2 4 6\n 3 6 9\n"
+# >> Table.new().generate_table(3) == standard
+# => true
+# doctest: Acceptance test without decoration
+# >> standard = "\n 1 2 3\n 2 4 6\n 3 6 9\n"
+# >> Table.new().generate_table(3, '', '') == standard
+# => true
+#
