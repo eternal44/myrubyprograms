539e2d53447bd3f0da89b97180c390826e8848db
James Youn <jamesyoun710@gmail.com>
2015-08-20 17:19:24 -0700

    Rebuild temperature_conversions.rb
    Use Kelvin as medium for all conversions

--- 1_week/2c_convert_challenge/lib/2c_convert_challenge.rb
+++ 1_week/2c_convert_challenge/lib/2c_convert_challenge.rb
@@ -39,7 +39,17 @@ class ConvertTemperature
       @conversion_to_make = kelvin_to_fahrenheit(@value)
       original_unit = "Kelvin"
       converted_unit = "Fahrenheit"
+    when 7
+      @conversion_to_make = kelvin_to_rankine(@value)
+      original_unit = "Kelvin"
+      converted_unit = "Rankine"
+    when 8
+      @conversion_to_make = rankine_to_kelvin(@value)
+      original_unit = "Kelvin"
+      converted_unit = "Rankine"
     end
+    # added more conversions in temperature_conversion.rb.
+    # add interface options later
 
     # return output to user
     converted_temperature = @conversion_to_make
--- 1_week/2c_convert_challenge/lib/temperature_conversions.rb
+++ 1_week/2c_convert_challenge/lib/temperature_conversions.rb
@@ -1,10 +1,12 @@
 module TemperatureConversions
   def celcius_to_fahrenheit(value)
-    value * 9.0 / 5 + 32
+    value_holder = celcius_to_kelvin(value)
+    kelvin_to_fahrenheit(value_holder)
   end
 
   def fahrenheit_to_celcius(value)
-    (value - 32) * (5 / 9.0)
+    value_holder = fahrenheit_to_kelvin(value)
+    kelvin_to_celcius(value_holder)
   end
 
   def celcius_to_kelvin(value)
@@ -12,8 +14,7 @@ module TemperatureConversions
   end
 
   def fahrenheit_to_kelvin(value)
-    value_holder = fahrenheit_to_celcius(value)
-    celcius_to_kelvin(value_holder)
+    (value + 459.67) * 5 / 9
   end
 
   def kelvin_to_celcius(value)
@@ -21,7 +22,35 @@ module TemperatureConversions
   end
 
   def kelvin_to_fahrenheit(value)
-    value_holder = kelvin_to_celcius(value)
-    celcius_to_fahrenheit(value_holder)
+    (value * 9 / 5.0) - 459.67
   end
+
+  def kelvin_to_rankine(value)
+    value * 1.8
+  end
+
+  def rankine_to_kelvin(value)
+    value / 1.8
+  end
+
+  def rankine_to_celcius(value)
+    value_holder = rankine_to_kelvin(value)
+    kelvin_to_celcius(value_holder)
+  end
+
+  def rankine_to_fahrenheit(value)
+    value_holder = rankine_to_kelvin(value)
+    kelvin_to_fahrenheit(value_holder)
+  end
+
+  def celcius_to_rankine(value)
+    value_holder = celcius_to_kelvin(value)
+    kelvin_to_rankine(value_holder)
+  end
+
+  def fahrenheit_to_rankine(value)
+    value_holder = fahrenheit_to_kelvin(value)
+    kelvin_to_rankine(value_holder)
+  end
+
 end
