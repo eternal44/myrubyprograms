4852132e9079270170ca60ed2edc3e0e27d0f203
James Youn <jamesyoun710@gmail.com>
2015-08-23 16:04:58 -0700

    Refactor to include correct formatting & input

--- 2_week/3c_column-string_alignment.rb
+++ 2_week/3c_column-string_alignment.rb
@@ -2,13 +2,13 @@
 
 class ListGenerator
   def column_alignment(lines)
+    lines = lines.split("\n")
     lines.map.with_index(1) do |output, line_number|
-      "Line #{number_spacing(lines, line_number)}#{line_number}:"\ # rewrite
-      "#{line_spacing(output, lines)}#{output}"                    # rewrite
+      "Line #{number_spacing(lines, line_number)}#{line_number}:  "\
+      "#{output}"
 
     end
   end
-
 
   private
 
@@ -16,19 +16,21 @@ class ListGenerator
       max_line_digits = lines.count.to_s.size
       " " *  (max_line_digits - line_number.to_s.size)
     end
-
-    def line_spacing(output, lines)
-      longest_value = lines.group_by(&:size).max.first
-      " " * (longest_value - output.size + 1)
-    end
 end
 
 if __FILE__ == $PROGRAM_NAME
-  lines = []
-  5.times do
-    lines <<  ["big", "ho", "big", "elephant", "z"]
-  end
-  lines = lines.flatten
+  lines = "I think the challenge states to split a string. Which may
+be programmers speech, but should probably have been presented as
+split a text.
 
-  puts ListGenerator.new().column_alignment(lines)
+So you would be missing a step here, I think.
+
+Your names are right for the code, I think. But that is because you
+read into being handed a string.
+
+Think about wanting to present a book with numbered lines.
+
+When I pulled your repository and ran the program, it looks like a list of words, rather than a line."
+
+puts ListGenerator.new().column_alignment(lines)
 end
