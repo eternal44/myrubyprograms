5217b9a862ab0af8a50c0a6c02aa1c616df96558
James Youn <jamesyoun710@gmail.com>
2015-08-23 15:02:30 -0700

    Remove "if" statement
    Description, please reference the Issue or feature, along with detailed
    information of the idea of the commit, remember, you may need to read this
    later to get re-informed of this commit.

--- 2_week/2c_formal_logic_challenge.rb
+++ 2_week/2c_formal_logic_challenge.rb
@@ -5,7 +5,13 @@
 # >> ternary(3 < 1,"hello",3)
 # => 3
 
+# no "if" statement
 def ternary(statement, true_result, false_result)
-  (true_result if statement == true) ||
-  (false_result if statement == false)
+  (true && statement) && true_result || false_result
 end
+
+# "if" statement
+# def ternary(statement, true_result, false_result)
+#   (true_result if statement == true) ||
+#   (false_result if statement == false)
+# end
