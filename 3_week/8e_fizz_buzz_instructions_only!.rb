5d60844c26157c7e19499b90e63da13b0890e384
James Youn <jamesyoun710@gmail.com>
2015-08-26 23:56:15 -0700

    Refactor 3_week/8e_fizz_buzz.rb
    Make it callable as a method
    Returns value instead of putting it

--- 3_week/8e_fizz_buzz_instructions_only!.rb
+++ 3_week/8e_fizz_buzz_instructions_only!.rb
@@ -1,6 +1,12 @@
-100.times do |number|
-  fizz = number % 3 == 0 ? 'fizz' : nil
-  buzz = number % 5 == 0 ? 'buzz' : nil
-  (puts fizz || buzz) ? "#{fizz}#{buzz}" : number
+def fizz_buzz(start, finish)
+  (start..finish).reduce("") do |b, number|
+    fizz = number % 3 == 0 ? 'fizz' : nil
+    buzz = number % 5 == 0 ? 'buzz' : nil
+    b + "#{fizz || buzz ? "#{fizz}#{buzz}" : number.to_s}" + "\n"
+  end
+end
+
+if __FILE__ == $PROGRAM_NAME
+  puts fizz_buzz(-10, 10)
 end
 
