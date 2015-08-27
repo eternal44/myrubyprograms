157ed38f8a88613a5675c6418b2a91c60ae3e4e3
James Youn <jamesyoun710@gmail.com>
2015-08-27 15:28:47 -0700

    Refactor named arguments & default arguments out
    to create a more flexible fizz_buzz method

--- 3_week/1c_FizzBuzz.rb
+++ 3_week/1c_FizzBuzz.rb
@@ -1,51 +1,44 @@
-# def fizz_buzz(number, rules = { 3 => 'Fizz', 5 => 'Buzz'}, fizz: 3, buzz: 5, **user_rules)
-# end
-#
-# list_of_num  # Who is responsible for list of numbers?  In the programming context...
-#
-# class / method
-#
-# result
-
 # doctest: fizz_buzz 1 returns 1
 # >> fizz_buzz(1)
 # => "1"
 # doctest: fizz_buzz 3 returns 'Fizz'
-# >> fizz_buzz(3)
+# >> fizz_buzz(3, fizz: 3)
 # => "Fizz"
 # doctest: fizz_buzz 5 returns 'Buzz'
-# >> fizz_buzz(5)
+# >> fizz_buzz(5, buzz: 5)
 # => "Buzz"
 # doctest: fizz_buzz 15 returns 'FizzBuzz'
-# >> fizz_buzz(15)
+# >> fizz_buzz(15, fizz: 3, buzz: 5)
 # => "FizzBuzz"
 # doctest: fizz_buzz custom rules for fizz with 2
 # >> fizz_buzz(2, fizz: 2)
 # => "Fizz"
 # doctest: fizz_buzz custom rules for fizz with 2 given 10
-# >> fizz_buzz(10, fizz: 2)
+# >> fizz_buzz(10, fizz: 2, buzz: 5)
 # => "FizzBuzz"
 # doctest: fizz_buzz custom rules for other_rules
 # >> fizz_buzz(7, dazz: 7)
 # => "Dazz"
 
-def fizz_buzz(num, number: num, fizz: 3, buzz: 5, **other_rules)
-  rules = {fizz: fizz, buzz: buzz}.merge(other_rules)
+# pass 2 arguments:
+# 1st arguement:  a number to test
+# 2nd argument:  a hash with a key variable that sets the word
+#                output and a value to divide the number (1st
+#                argument) by
+
+def fizz_buzz(num, **rules)
     result = ""
     rules.each_value do |v|
-      result << "#{rules.key(v).to_s.capitalize}" if number % v == 0
+      result << "#{rules.key(v).to_s.capitalize}" if num % v == 0
     end
     result.empty? ? num.to_s : result
 end
 
 if __FILE__ == $PROGRAM_NAME
   -15.upto(15) do |num|
-  puts fizz_buzz(num, boogy: 4)
+    puts fizz_buzz(num, boogy: 4, negy: -7)
   end
 end
 __END__
 
-# method documention how do you suply the optional rules, what are the
-# expectatoins to make it work.
-#
 fizz_buzz(1 fizz: 3, buzz: 5, **option_rules )
