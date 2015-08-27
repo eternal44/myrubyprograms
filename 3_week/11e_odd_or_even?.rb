5e6be2bd4cc1810e692be29a54775dae82422441
James Youn <jamesyoun710@gmail.com>
2015-08-27 16:19:20 -0700

    Redesign to take single integers
    Used to take arrays as an argument.  For flexibility & max usibility
    loop through an array with this method instead of taking on the whole
    array as an argument.
    
    This is to maximize the usability of this method

--- 3_week/11e_odd_or_even?.rb
+++ 3_week/11e_odd_or_even?.rb
@@ -1,11 +1,14 @@
-def even_or_odd(array)
-  array.each do |number|
-    output = number.even? ? 'even' : 'odd'
-    puts "#{number} is #{output}"
-  end
+# doctest: gets even
+# >> even_or_odd(3)
+# => "3 is odd"
+def even_or_odd(number)
+  output = number.even? ? 'even' : 'odd'
+  "#{number} is #{output}"
 end
 
 if __FILE__ == $PROGRAM_NAME
   array = [12, 23, 456, 123, 4579]
-  even_or_odd(array)
+  array.each do |num|
+    puts even_or_odd(num)
+  end
 end
