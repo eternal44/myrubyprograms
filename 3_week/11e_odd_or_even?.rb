8285924dcf7fb12a94653713e1f7e14be76640b7
James Youn <jamesyoun710@gmail.com>
2015-08-28 01:42:44 -0700

    Refactor to return a pure result
    Build a reader-friendly message in a separate reporting part of the
    script.

--- 3_week/11e_odd_or_even?.rb
+++ 3_week/11e_odd_or_even?.rb
@@ -1,14 +1,13 @@
 # doctest: gets even
 # >> even_or_odd(3)
-# => "3 is odd"
+# => 'odd'
 def even_or_odd(number)
-  output = number.even? ? 'even' : 'odd'
-  "#{number} is #{output}"
+  number.even? ? 'even' : 'odd'
 end
 
 if __FILE__ == $PROGRAM_NAME
   array = [12, 23, 456, 123, 4579]
   array.each do |num|
-    puts even_or_odd(num)
+    puts "#{num} is #{even_or_odd(num)}"
   end
 end
