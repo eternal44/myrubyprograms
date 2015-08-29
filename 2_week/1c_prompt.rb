cf8d4a881f2a84857cc366e27edbb5f0457adc16
James Youn <jamesyoun710@gmail.com>
2015-08-29 12:20:39 -0700

    Starting point for expanded prompts
    User supplied
    - methods
    - default resonses

--- 2_week/1c_prompt.rb
+++ 2_week/1c_prompt.rb
@@ -1,21 +1,30 @@
 # add line numbers
-def prompt(quest = '', question: quest, symbol: '>>')
+def prompt(
+  quest = '', question: quest, symbol: '>>', chomp: false, apply_methods: [],
+  default_answer: nil
+          )
   puts quest + '?'
-  print symbol
+  print "#{symbol}#{default_answer}"
 
   print '  '
-  gets
-
+  answer = gets
+  apply_methods.each do | m |
+    answer = answer.send(m) if answer.respond_to?(m)
+  end
+  answer
 end
 
 if __FILE__ == $PROGRAM_NAME
 answers = []
 
-questions = ['What is your name', 'What is your age',
-             'What is your favorite color']
+questions = [
+  ['What is your name', apply_methods: [:chomp]], ['What is your age', apply_methods: [:to_i]],
+  ['What is your favorite color'],
+  ['Would you like to order desert?', symbol: 'Y/n', default_answer: 'Y'],
+]
 
-questions.each do |q|
-  answers << prompt(q, symbol: '--')
+questions.each do |q, *options|
+  answers << prompt(q, *options)
 end
 puts answers
 end
