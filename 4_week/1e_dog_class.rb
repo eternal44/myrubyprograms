00865fb924961650ab6d6b03e673f4ac3da0d9df
James Youn <jamesyoun710@gmail.com>
2015-08-30 13:37:22 -0700

    Dog class with simple methods
    Also includ 'attr_accessor' to play with
    
    Delete empty file:  analyzer.rb

---
+++ 4_week/1e_dog_class.rb
@@ -0,0 +1,19 @@
+class Dog
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+
+  def bark
+    'Bow wow!'
+  end
+
+  def eat
+    'Nom nom nom....'
+  end
+
+  def chase_cat
+    'Here kitty kitty'
+  end
+end
